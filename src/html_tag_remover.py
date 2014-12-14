58e1f9cc3bf62361fcb9268c1f1387d2c3dd650f
Chaitanya Datye <chaitanyadatye@wirelessprvnat-172-17-80-93.near.illinois.edu>
2014-12-13 20:30:55 -0600

    Input file and modified

--- src/html_tag_remover.py
+++ src/html_tag_remover.py
@@ -10,9 +10,7 @@ def cleanup_html(text):
     taglist1 = ['code']
     t1 = remove_html_tag_content(text,taglist1);
     t2 = strip_tags(text,taglist2);
-    t2 = t2[12:]
-    t2 = t2[:-14]
-    return t2;
+    return t2.get_text();
 
 def filter_common_words(text, stop_words):
     if isinstance(text, unicode):
