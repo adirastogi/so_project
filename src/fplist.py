bc83b69dce6457e83842668bb4327e6446904e97
Chaitanya Datye <csdatye.92@gmail.com>
2014-12-16 18:34:48 -0600

    Changed fplist

--- src/fplist.py
+++ src/fplist.py
@@ -1,11 +1,4 @@
-import html_tag_remover
 import sys
-import csv as csv
-import os
-import fileinput
-import nltk
-import string
-from nltk.corpus import stopwords
 
 def main():
     filename = sys.argv[1]
